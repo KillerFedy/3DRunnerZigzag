6edd161f6678877ea7ce4c9301039304cf54db0f
Фёдор <zuev.f.s@gmail.com>
2020-11-11 17:28:21 +1000

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _playerRigidbody;
+    private float _speed = 5f; 
+    private bool _isMovingRight = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _playerRigidbody.velocity = new Vector3 (_speed, 0f,0f);
+        if (Input.GetMouseButtonDown(0))
+        {
+            ChangeDirection();   
+        }
+        if (_isMovingRight) {
+            _playerRigidbody.velocity = new Vector3 (_speed, 0f, 0f);
+        } 
+        else {
+            _playerRigidbody.velocity = new Vector3 (0f, 0f, _speed);
+        }
+
+    }
+    void ChangeDirection()
+    {
+        _isMovingRight =  !_isMovingRight;
+    }
+
+}
